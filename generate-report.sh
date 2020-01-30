8704ad7e88fd0ce3bee925fa989fe2024d8d1e35
jhpoelen <jhpoelen@xs4all.nl>
2020-01-30 13:59:57 -0800

    setup travis config for reports

--- generate-report.sh
+++ generate-report.sh
@@ -9,26 +9,24 @@
 # Prerequisites: Java and https://github.com/globalbioticinteractions/elton .
 # 
 
-
-
 TODAY=`date --iso-8601`
 REPORT_DIR=output/$TODAY
 mkdir -p $REPORT_DIR
-REVIEW=$REPORT_DIR/review.tsv
-REVIEW_BY_COLLECTION=$REPORT_DIR/review_by_collection.tsv
+REVIEW=$REPORT_DIR/review_notes.tsv
+REVIEW_BY_COLLECTION=$REPORT_DIR/review_notes_by_collection.tsv
 INTERACTIONS=$REPORT_DIR/interactions.tsv
 INTERACTIONS_BY_COLLECTION=$REPORT_DIR/interactions_by_collection.tsv
 
-echo "updating TPT affiliated elton datasets"
+# updating TPT affiliated elton datasets
 #cat datasets.tsv | xargs elton update 
 
-echo "generating review reports"
+# generating review reports
 cat datasets.tsv | xargs elton review --type note > $REVIEW
 
-echo "generating interaction data"
+# generating interaction data
 cat datasets.tsv | xargs elton interactions > $INTERACTIONS
 
-echo "group review notes by collection"
+# group review notes by collection
 echo -e "#notes\tcollectionCode\tnote" > $REVIEW_BY_COLLECTION
 cat $REVIEW | tail -n+2 | awk -F '\t' '{ print $9 "\t" $6 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
 
@@ -36,5 +34,12 @@ echo "group interaction data by collection"
 echo -e "#records\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTIONS_BY_COLLECTION
 cat $INTERACTIONS | tail -n+2 | awk -F '\t' '{ print $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
 
-echo "contents of $REPORT_DIR:"
-ls -1 $REPORT_DIR
+echo "---- interaction record by interaction type by collection"
+cat $INTERACTIONS_BY_COLLECTION
+
+echo -e "\n---- review notes by collection"
+cat $REVIEW_BY_COLLECTION
+
+echo -e "\nfor more information, see $REPORT_DIR"
+
+
