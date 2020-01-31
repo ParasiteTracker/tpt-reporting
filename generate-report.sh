047810c241aae2702ae641d21ff7f27e07705c19
jhpoelen <jhpoelen@xs4all.nl>
2020-01-30 18:24:11 -0800

    increase mem; add namespace; process line-by-line to avoid premature exit of reviews

--- generate-report.sh
+++ generate-report.sh
@@ -27,18 +27,18 @@ else
 fi
 
 # generating review reports
-cat datasets.tsv | xargs elton review --type note > $REVIEW
+cat datasets.tsv | xargs -L1 java -Xmx4G -jar $(which elton) review --type note >> $REVIEW
 
 # generating interaction data
-cat datasets.tsv | xargs elton interactions > $INTERACTIONS
+cat datasets.tsv | xargs -L1 java -Xmx4G -jar $(which elton) interactions >> $INTERACTIONS
 
 # group review notes by collection
-echo -e "#notes\tinstitutionCode\tcollectionCode\tnote" > $REVIEW_BY_COLLECTION
-cat $REVIEW | tail -n+2 | awk -F '\t' '{ print $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
+echo -e "#notes\tnamespace\tinstitutionCode\tcollectionCode\tnote" > $REVIEW_BY_COLLECTION
+cat $REVIEW | tail -n+2 | awk -F '\t' '{ print $4 "\t" $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
 
 # group interaction data by collection
-echo -e "#records\tinstitutionCode\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTIONS_BY_COLLECTION
-cat $INTERACTIONS | tail -n+2 | awk -F '\t' '{ print $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
+echo -e "#records\tnamespace\tinstitutionCode\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTIONS_BY_COLLECTION
+cat $INTERACTIONS | tail -n+2 | awk -F '\t' '{ print $46 "\t" $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
 
 echo -e "\n---- interaction record by interaction type by collection ----"
 cat $INTERACTIONS_BY_COLLECTION
