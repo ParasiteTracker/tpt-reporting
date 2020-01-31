f3b8f949a99f0ea47d7e03fb5d852ab84d775e48
jhpoelen <jhpoelen@xs4all.nl>
2020-01-31 10:58:44 -0800

    strip headers; add report zip name

--- generate-report.sh
+++ generate-report.sh
@@ -26,19 +26,23 @@ else
   cat datasets.tsv | xargs java -Xmx4G -jar $(which elton) update
 fi
 
+echo -e "\n"
+
 # generating review reports
-cat datasets.tsv | xargs -L1 java -Xmx4G -jar $(which elton) review --type note >> $REVIEW
+cat datasets.tsv | xargs -L1 java -Xmx4G -jar $(which elton) review --type note | tail -n+2 >> $REVIEW
+
+echo -e "\n"
 
 # generating interaction data
-cat datasets.tsv | xargs -L1 java -Xmx4G -jar $(which elton) interactions >> $INTERACTIONS
+cat datasets.tsv | xargs -L1 java -Xmx4G -jar $(which elton) interactions | tail -n+2 >> $INTERACTIONS
 
 # group review notes by collection
 echo -e "#notes\tnamespace\tinstitutionCode\tcollectionCode\tnote" > $REVIEW_BY_COLLECTION
-cat $REVIEW | tail -n+2 | awk -F '\t' '{ print $4 "\t" $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
+cat $REVIEW | awk -F '\t' '{ print $4 "\t" $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
 
 # group interaction data by collection
 echo -e "#records\tnamespace\tinstitutionCode\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTIONS_BY_COLLECTION
-cat $INTERACTIONS | tail -n+2 | awk -F '\t' '{ print $46 "\t" $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
+cat $INTERACTIONS | awk -F '\t' '{ print $46 "\t" $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
 
 echo -e "\n---- interaction record by interaction type by collection ----"
 cat $INTERACTIONS_BY_COLLECTION
@@ -49,5 +53,5 @@ cat $REVIEW_BY_COLLECTION
 echo -e "\nFor more information, see $PWD/$REPORT_DIR"
 
 zip $REPORT_ARCHIVE $REPORT_DIR/*
-echo -e "\nDownload the full report using single-use, and expiring, file.io link at:"
+echo -e "\nDownload the full report [$REPORT_ARCHIVE] using single-use, and expiring, file.io link at:"
 curl -F "file=@$REPORT_ARCHIVE" https://file.io 
