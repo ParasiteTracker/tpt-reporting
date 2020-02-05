b09b00f0b0eab76c7f6e3922185435eef7360427
jhpoelen <jhpoelen@xs4all.nl>
2020-02-05 07:12:37 -0800

    attempt to make reports work on mac os x

--- generate-report.sh
+++ generate-report.sh
@@ -33,20 +33,20 @@ cat datasets.tsv | xargs elton update
 echo -e "\n"
 
 # generating review reports
-cat datasets.tsv | xargs -L1 elton review --type note | tail -n+2 >> $REVIEW
+cat datasets.tsv | xargs -L1 elton review --type note | tail -n +2 >> $REVIEW
 
 echo -e "\n"
 
 # generating interaction data
-cat datasets.tsv | xargs -L1 elton interactions | tail -n+2 >> $INTERACTIONS
+cat datasets.tsv | xargs -L1 elton interactions | tail -n +2 >> $INTERACTIONS
 
 # group review notes by collection
 echo -e "#notes\tnamespace\tinstitutionCode\tcollectionCode\tnote" > $REVIEW_BY_COLLECTION
-cat $REVIEW | awk -F '\t' '{ print $4 "\t" $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
+cat $REVIEW | awk -F '\t' '{ print $4 "\t" $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
 
 # group interaction data by collection
 echo -e "#records\tnamespace\tinstitutionCode\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTIONS_BY_COLLECTION
-cat $INTERACTIONS | awk -F '\t' '{ print $46 "\t" $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed 's/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
+cat $INTERACTIONS | awk -F '\t' '{ print $46 "\t" $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
 
 echo -e "\n---- interaction record by interaction type by collection ----"
 cat $INTERACTIONS_BY_COLLECTION
