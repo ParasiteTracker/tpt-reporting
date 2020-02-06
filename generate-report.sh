b428ce916e2dabefb9855655408e609f57dabf99
jhpoelen <jhpoelen@xs4all.nl>
2020-02-06 07:59:19 -0800

    add review summary; added name report; added interaction type report

--- generate-report.sh
+++ generate-report.sh
@@ -17,8 +17,11 @@ REPORT_DIR=output/$TODAY
 REPORT_ARCHIVE=output/tpt-globi-report-$TODAY.zip
 mkdir -p $REPORT_DIR
 REVIEW=$REPORT_DIR/review_issues.tsv
+REVIEW_SUMMARY=$REPORT_DIR/review_summary.tsv
 REVIEW_BY_COLLECTION=$REPORT_DIR/review_issues_by_collection.tsv
-INTERACTIONS=$REPORT_DIR/interactions.tsv
+INTERACTIONS=$REPORT_DIR/indexed-interactions.tsv
+NAMES=$REPORT_DIR/indexed-names.tsv
+INTERACTION_TYPES_BY_COLLECTION=$REPORT_DIR/interaction_types_by_collection.tsv
 INTERACTIONS_BY_COLLECTION=$REPORT_DIR/interactions_by_collection.tsv
 
 echo "using elton version $(elton version)"
@@ -33,25 +36,37 @@ cat datasets.tsv | xargs elton update
 echo -e "\n"
 
 # generating review reports
-cat datasets.tsv | xargs -L1 elton review --type note | tail -n +2 >> $REVIEW
+cat datasets.tsv | xargs -L1 elton review --type note,summary | tail -n +2 >> $REVIEW
+
+cat $REVIEW | cut -f1-6 | grep "summary" > $REVIEW_SUMMARY
 
 echo -e "\n"
 
 # generating interaction data
 cat datasets.tsv | xargs -L1 elton interactions | tail -n +2 >> $INTERACTIONS
 
+# generate names
+cat datasets.tsv | xargs -L1 elton names | tail -n +2 >> $NAMES
+
 # group review issues by collection
-echo -e "#issues\tnamespace\tinstitutionCode\tcollectionCode\tnote" > $REVIEW_BY_COLLECTION
-cat $REVIEW | awk -F '\t' '{ print $4 "\t" $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
+echo -e "#issues\tinstitutionCode\tcollectionCode\tnote" > $REVIEW_BY_COLLECTION
+cat $REVIEW | awk -F '\t' '{ print $9 "\t" $10 "\t" $6 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' >> $REVIEW_BY_COLLECTION
 
 # group interaction data by collection
-echo -e "#records\tnamespace\tinstitutionCode\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTIONS_BY_COLLECTION
-cat $INTERACTIONS | awk -F '\t' '{ print $46 "\t" $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
+echo -e "#records\tinstitutionCode\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTIONS_BY_COLLECTION
+cat $INTERACTIONS | awk -F '\t' '{ print $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' >> $INTERACTIONS_BY_COLLECTION
 
-echo -e "\n---- interaction record by interaction type by collection ----"
+echo -e "\n---- interaction types by institution/collection ----"
+echo -e "#institutionCode\tcollectionCode\tinteractionTypeId\tinteractionTypeName" > $INTERACTION_TYPES_BY_COLLECTION
+cat $INTERACTIONS | awk -F '\t' '{ print $5 "\t" $4 "\t" $18 "\t" $19 }' | sort | uniq | sort >> $INTERACTION_TYPES_BY_COLLECTION
+
+echo -e "\n---- interaction record count by interaction type by collection ----"
+cat $INTERACTION_TYPES_BY_COLLECTION
+
+echo -e "\n---- interaction record count by interaction type by collection ----"
 cat $INTERACTIONS_BY_COLLECTION
 
-echo -e "\n---- review notes by collection ----"
+echo -e "\n---- review notes by institution/collection ----"
 cat $REVIEW_BY_COLLECTION
 
 echo -e "\nFor more information, see $PWD/$REPORT_DIR"
