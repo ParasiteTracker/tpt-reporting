5f8589a21bf0612e0ac46e6d7f1e6a4d883f220b
jhpoelen <jhpoelen@xs4all.nl>
2020-02-07 08:15:51 -0800

    fix param default

--- generate-report.sh
+++ generate-report.sh
@@ -22,7 +22,7 @@ INTERACTIONS_FULL="$REPORT_DIR/indexed_interactions_full.tsv.gz"
 INTERACTIONS_SIMPLE="$REPORT_DIR/indexed_interactions_simple.tsv.gz"
 DATASET_INFO="$REPORT_DIR/datasets_under_review.tsv"
 
-DATASET_LIST_FILE=${:-datasets.tsv}
+DATASET_LIST_FILE=${1:-datasets.tsv}
 DATASETS_UNDER_REVIEW="$(cat $DATASET_LIST_FILE)"
 DATASETS_UNDER_REVIEW_HEAD="$(head -n1 $DATASET_LIST_FILE)"
 DATASETS_UNDER_REVIEW_TAIL="$(tail -n+2 $DATASET_LIST_FILE)"
