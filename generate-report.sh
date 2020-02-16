68e37639758d8b6d9a8af12c4db93affdecdfbbe
jhpoelen <jhpoelen@xs4all.nl>
2020-02-16 03:30:43 -0800

    refactor report script

--- generate-report.sh
+++ generate-report.sh
@@ -45,47 +45,55 @@ function updateAll {
   done
 }
 
-updateAll
 
 # generating review reports
+function reviewDatasets {
+  echo "${DATASETS_UNDER_REVIEW_HEAD}" | xargs -L1 $ELTON_CMD review --type info,note | gzip > "$REVIEW"
+  echo "${DATASETS_UNDER_REVIEW_TAIL}" | xargs -L1 $ELTON_CMD review --no-header --type info,note | tail -n +2 | gzip >> "$REVIEW"
 
-echo "${DATASETS_UNDER_REVIEW_HEAD}" | xargs -L1 $ELTON_CMD review --type info,note | gzip > "$REVIEW"
-echo "${DATASETS_UNDER_REVIEW_TAIL}" | xargs -L1 $ELTON_CMD review --no-header --type info,note | tail -n +2 | gzip >> "$REVIEW"
-
-echo -e "\n"
-
-# generating interaction data
-echo "$DATASETS_UNDER_REVIEW_HEAD" | xargs -L1 $ELTON_CMD interactions | gzip > "$INTERACTIONS_FULL"
-echo "$DATASETS_UNDER_REVIEW_TAIL" | xargs -L1 $ELTON_CMD interactions --no-header | gzip >> "$INTERACTIONS_FULL"
-
-# group review issues by collection
-echo -e "institutionCode\tcollectionId\tcollectionCode\tsourceCitation\tdistinctReviewCommentCount\ttype\tcomment" > "$REVIEW_BY_COLLECTION"
+  echo -e "\n"
+  # group review issues by collection
+  echo -e "institutionCode\tcollectionId\tcollectionCode\tsourceCitation\tdistinctReviewCommentCount\ttype\tcomment" > "$REVIEW_BY_COLLECTION"
 cat $REVIEW | gunzip | tail -n +2 | awk -F '\t' '{ print $9 "\t" $11 "\t" $10 "\t" $14 "\t" $5 "\t" $6 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' | awk -F '\t' '{ print $2 "\t" $3 "\t" $4 "\t" $5 "\t" $1 "\t" $6 "\t" $7 }' | sed -E $'s/\tnote\t/\tissue\t/g' | sort >> "$REVIEW_BY_COLLECTION"
 
-# review summary
-echo -e "distinctReviewCommentCount\ttype\tcomment" > "$REVIEW_SUMMARY"
-cat $REVIEW | gunzip | tail -n +2 | awk -F '\t' '{ print $5 "\t" $6 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' | sed -E $'s/\tnote\t/\tissue\t/g' | sort >> "$REVIEW_SUMMARY"
+  # review summary
+  echo -e "distinctReviewCommentCount\ttype\tcomment" > "$REVIEW_SUMMARY"
+  cat $REVIEW | gunzip | tail -n +2 | awk -F '\t' '{ print $5 "\t" $6 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' | sed -E $'s/\tnote\t/\tissue\t/g' | sort >> "$REVIEW_SUMMARY"
 
-# group interaction data by collection
-echo -e "institutionCode\tcollectionId\tcollectionCode\tsourceCitation\tindexedInteractionRecordCount\tinteractionTypeName\tinteractionTypeId" > "$INTERACTIONS_BY_COLLECTION"
-cat "$INTERACTIONS_FULL" | gunzip | tail -n +2 | awk -F '\t' '{ print $6 "\t" $5 "\t" $4 "\t" $49 "\t" $20 "\t" $19 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' | awk -F '\t' '{ print $2 "\t" $3 "\t" $4 "\t" $5 "\t" $1  "\t" $6 "\t" $7 }' | sort >> "$INTERACTIONS_BY_COLLECTION"
+}
 
-cat "$INTERACTIONS_FULL" | gunzip | awk -F '\t' '{ print $6 "\t" $5 "\t" $4 "\t" $3 "\t" $8 "\t" $20 "\t" $27 }' | gzip > "$INTERACTIONS_SIMPLE"
 
-echo -e "\n---- distinct review comments by type ----"
-cat "$REVIEW_SUMMARY"
+function indexInteractionData {
+  # generating interaction data
+  echo "$DATASETS_UNDER_REVIEW_HEAD" | xargs -L1 $ELTON_CMD interactions | gzip > "$INTERACTIONS_FULL"
+  echo "$DATASETS_UNDER_REVIEW_TAIL" | xargs -L1 $ELTON_CMD interactions --no-header | gzip >> "$INTERACTIONS_FULL"
 
-echo -e "\n---- indexed interaction record count by institutionCode, collectionId, collectionCode, and interaction type ----"
+  # group interaction data by collection
+  echo -e "institutionCode\tcollectionId\tcollectionCode\tsourceCitation\tindexedInteractionRecordCount\tinteractionTypeName\tinteractionTypeId" > "$INTERACTIONS_BY_COLLECTION"
+  cat "$INTERACTIONS_FULL" | gunzip | tail -n +2 | awk -F '\t' '{ print $6 "\t" $5 "\t" $4 "\t" $49 "\t" $20 "\t" $19 }' | sort | uniq -c | sort -nr | sed -E $'s/[ ]*//;s/[ ]/\t/' | awk -F '\t' '{ print $2 "\t" $3 "\t" $4 "\t" $5 "\t" $1  "\t" $6 "\t" $7 }' | sort >> "$INTERACTIONS_BY_COLLECTION"
+
+  cat "$INTERACTIONS_FULL" | gunzip | awk -F '\t' '{ print $6 "\t" $5 "\t" $4 "\t" $3 "\t" $8 "\t" $20 "\t" $27 }' | gzip > "$INTERACTIONS_SIMPLE"
+}
+
+
+function printReports() {
+  echo -e "\n---- distinct review comments by type ----"
+  cat "$REVIEW_SUMMARY"
+
+  echo -e "\n---- indexed interaction record count by institutionCode, collectionId, collectionCode, and interaction type ----"
 cat "$INTERACTIONS_BY_COLLECTION"
 
-echo -e "\n---- distinct review comment count by institution, collection and review comment type ----"
-cat "$REVIEW_BY_COLLECTION"
+  echo -e "\n---- distinct review comment count by institution, collection and review comment type ----"
+  cat "$REVIEW_BY_COLLECTION"
 
-echo "$DATASETS_UNDER_REVIEW" | xargs -L1 $ELTON_CMD datasets > "$DATASET_INFO"
+  echo "$DATASETS_UNDER_REVIEW" | xargs -L1 $ELTON_CMD datasets > "$DATASET_INFO"
 
-DATASET_REFERENCES=$(cat $DATASET_INFO | cut -f2,3,4 | tail -n +2 | sed -E 's/\t/ accessed via /' | sed -E 's/^/ - /g' | sed -E 's/\t/ on /')
+  DATASET_REFERENCES=$(cat $DATASET_INFO | cut -f2,3,4 | tail -n +2 | sed -E 's/\t/ accessed via /' | sed -E 's/^/ - /g' | sed -E 's/\t/ on /')
+}
 
-cat <<EOF > "$REPORT_DIR/README"
+
+function generateReadme {
+  cat <<EOF > "$REPORT_DIR/README"
 GloBI Data Review Report
 
 Datasets under review:
@@ -134,18 +142,33 @@ datasets_under_review.tsv:
 
 EOF
 
+}
+
+
 echo -e "\nFor more information, see $REPORT_DIR"
 
-NUMBER_OF_INTERACTIONS=$(cat "$INTERACTIONS_FULL" | gunzip | sort | uniq | wc -l)
 
-if [ $NUMBER_OF_INTERACTIONS -gt 1 ]
-then
+function publishReview {
   OLD_DIR=$PWD
   cd "$REPORT_DIR"
   zip "$REPORT_ARCHIVE" README review_summary.tsv review_summary_by_collection.tsv review_comments.tsv.gz indexed_interactions_full.tsv.gz indexed_interactions_simple.tsv.gz datasets_under_review.tsv 
   cd "$OLD_DIR"
   echo -e "\nDownload the full report [$REPORT_ARCHIVE] using single-use, and expiring, file.io link at:"
   curl -F "file=@$REPORT_ARCHIVE" https://file.io 
+
+}
+
+updateAll
+reviewDatasets
+indexInteractionData
+printReports
+generateReadme
+
+NUMBER_OF_INTERACTIONS=$(cat "$INTERACTIONS_FULL" | gunzip | sort | uniq | wc -l)
+
+if [ $NUMBER_OF_INTERACTIONS -gt 1 ]
+then
+  publishReview
 else
   echo "Cannot create report because no interaction records were found. Please check log."
   echo "Please check you have the latest elton installed. You are using Elton v[$ELTON_VERSION]. See https://github.com/globalbioticinteractions/elton#install for install instructions and https://github.com/globalbioticinteractions/elton/releases/latest for latest version )."
